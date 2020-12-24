af6d796d1505e9c431e934cd65c168e6ade43193
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2020-12-24 15:01:54 +0530

    update till fetching values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import logo from "/images/bday.svg";
+import { useState } from "react";
 
 var bgColor = {
   purple: "#C7D2FE",
@@ -8,19 +9,65 @@ var bgColor = {
 };
 
 export default function App() {
+  var [userInput, setUserInput] = useState("7th?");
+
+  function inputChangeHandler(event) {
+    setUserInput(event.target.value);
+  }
+
+  var [newNumberFetch, setNumberFetch] = useState("7?");
+
+  function numberFetch(event) {
+    setNumberFetch(event.target.value);
+  }
+
+  function calculate() {
+    console.log(userInput);
+    console.log(newNumberFetch);
+  }
+
   return (
     // Textarea code
     <div className="App">
-      <h1>How Luck Is your Birthday?</h1>
+      <h1>How Lucky are you?</h1>
       <div style={{ backgroundColor: bgColor.purple }}>
         <p className="details">Enter Your Birthday below</p>
-        <textarea className="textStyle" placeholder="DD/MM/YYYY"></textarea>
+        <input
+          onChange={inputChangeHandler}
+          type="date"
+          className="textStyle"
+        ></input>
+        <div>
+          <p className="details">Enter Your Fav Number</p>
+          <input
+            onChange={numberFetch}
+            type="number"
+            className="textStyle"
+            placeholder="Input a number"
+          ></input>
+        </div>
+      </div>
+
+      <div style={{ backgroundColor: "#C7D2FE" }}>
+        <button onClick={calculate} className="myButton">
+          Let's go
+        </button>
       </div>
 
       {/* Adding Image */}
       <div style={{ backgroundColor: bgColor.lightPurple }}>
         <img className="imageSize" src={logo} alt="Logo" />
       </div>
+
+      {/* Showing Output */}
+      <div>
+        <p>
+          According to you, your Birthday is on{" "}
+          <span className="output">{userInput}</span> and your lucky number is{" "}
+          <span className="output">{newNumberFetch}</span>
+        </p>
+      </div>
+      <div>{}</div>
     </div>
   );
 }
